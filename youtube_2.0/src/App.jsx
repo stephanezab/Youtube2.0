b9a43ce23115e5cda409c1277d95fbee1bb4ec7f
stephanezab <stephanezab@gmail.com>
2024-08-05 18:24:49 -0500

    Update App.jsx

--- youtube_2.0/src/App.jsx
+++ youtube_2.0/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Box} from '@mui/material'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+  <Box sx ={{backgroundColor: "#000"}}>
+    Navbar
+    <Routes>
+        
+
+    </Routes>
+  </Box>
+  </BrowserRouter>
+    
+)
 
 export default App
