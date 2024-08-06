628ce70874986da863808c1479457047093f1af4
stephanezab <stephanezab@gmail.com>
2024-08-06 18:37:39 -0500

    update

--- youtube_2.0/src/components/Navbar.jsx
+++ youtube_2.0/src/components/Navbar.jsx
@@ -3,16 +3,18 @@ import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 import { logo} from '../utils/constants'
+import SearchBar from './SearchBar'
 
 const Navbar = () => (
   <Stack 
   direction="row" 
   alignItems="center" 
   p={2} 
-  sx={{ positon: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}>
+  sx={{ positon: 'sticky', background: 'blue', top: 0, justifyContent: 'space-between'}}>
     <Link to="/" style={{ display: 'flex', alignItems: 'center'}}>
     <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar/>
   </Stack>
 )
 
