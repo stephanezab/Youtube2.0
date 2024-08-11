49fc9c91fad8b052c1ae4434260116e19e6c6924
stephanezab <stephanezab@gmail.com>
2024-08-11 13:40:53 -0500

    update

--- youtube_2.0/src/components/Feed.jsx
+++ youtube_2.0/src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+import {Sidebar, Videos} from './'
 
 const Feed = () => {
   return (
