ad349c9207de1ae4d905cc4d36ca4db2789c250f
stephanezab <stephanezab@gmail.com>
2024-08-13 09:16:40 -0500

    updates

--- youtube_2.0/src/components/Sidebar.jsx
+++ youtube_2.0/src/components/Sidebar.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { Stack, colors } from '@mui/material'
 import { categories } from '../utils/constants'
 
-const selectCategory = "New" // to look at
 
-const Sidebar = () => {
+const Sidebar = ({ selectedCategory, setSelectedCategory}) => {
     return (
         <Stack
             direction='row'
@@ -17,12 +16,13 @@ const Sidebar = () => {
             {categories.map((category)=>(
                 <button
                 className='category-btn'
-                style={{background: category.name === selectCategory && "#FC1503"}}
+                onClick={()=>{setSelectedCategory(category.name)}}
+                style={{background: category.name === selectedCategory && "#FC1503"}}
                 key={category.name}   
                 >
-                    <span style={{color: category.name === selectCategory ? "white": "red", marginRight: "15px"}}>{category.icon}</span>
+                    <span style={{color: category.name === selectedCategory ? "white": "red", marginRight: "15px"}}>{category.icon}</span>
 
-                    <span style={{opacity: category.name === selectCategory ? "1": "0.8"}}>{category.name}
+                    <span style={{opacity: category.name === selectedCategory ? "1": "0.8"}}>{category.name}
                     </span>
                 </button>
             ))}
