2601d47c138713cf4e9b42fbe315aed0d61e08ee
stephanezab <stephanezab@gmail.com>
2024-08-13 19:58:47 -0500

    videocard update

--- youtube_2.0/src/components/Videos.jsx
+++ youtube_2.0/src/components/Videos.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Stack, Box} from '@mui/material'
 import {VideoCard, ChannelCard} from './'
+
+
 const Videos = ({videos}) => {
   return (
     <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
