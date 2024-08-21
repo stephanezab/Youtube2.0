7965d4995203534be8847febc41af110e44c3219
stephanezab <stephanezab@gmail.com>
2024-08-20 19:26:40 -0500

    Update ChannelDetail.jsx

--- youtube_2.0/src/components/ChannelDetail.jsx
+++ youtube_2.0/src/components/ChannelDetail.jsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Videos, ChannelCard  } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
   const {id} = useParams()
-  
+  console.log(channelDetail)
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]) )
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
