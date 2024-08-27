d705545dca3e02a3ebf60a291b3956998fdfbb7f
stephanezab <stephanezab@gmail.com>
2024-08-26 19:50:45 -0500

    Update Videos.jsx

--- youtube_2.0/src/components/Videos.jsx
+++ youtube_2.0/src/components/Videos.jsx
@@ -4,6 +4,7 @@ import {VideoCard, ChannelCard} from './'
 
 
 const Videos = ({videos, direction}) => {
+  if (!videos) return <div>Loading...</div>
   return (
     <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
 
