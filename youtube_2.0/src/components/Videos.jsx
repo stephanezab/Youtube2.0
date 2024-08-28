5f034f1fbc24e5c4f239f94d5ae5227c52fe51c4
stephanezab <stephanezab@gmail.com>
2024-08-28 16:33:20 -0500

    Update Videos.jsx

--- youtube_2.0/src/components/Videos.jsx
+++ youtube_2.0/src/components/Videos.jsx
@@ -8,13 +8,15 @@ const Videos = ({videos, direction}) => {
   return (
     <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
 
-      {videos.map((item, idx)=> (
-        <Box key={idx} sx={{ width: {xs:'100%',sm:'358px', md: '320px'}, maxWidth:'400'}}>
-          
-          {item.id.videoId && <VideoCard video={item}/>}
-          {item.id.channelId && <ChannelCard channelDetail={item}/>}
+      {videos.map((item, idx)=> {
+        const hascontent = item.id?.videoId || item.id?.channelId;
+
+        return hascontent? (<Box key={idx} sx={{ width: {xs:'100%',sm:'358px', md: '320px'}, maxWidth:'400', border:'solid 10px red'}}>
+          {item?.id?.videoId && <VideoCard video={item}/>}
+          {item?.id?.channelId && <ChannelCard channelDetail={item}/>}
         </Box>
-      ))}
+        ): null
+      })}
 
     </Stack>
   )
