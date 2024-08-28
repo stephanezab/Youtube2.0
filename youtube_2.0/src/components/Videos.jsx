f5e3a996ebfd331c19377b6bb25c9e6308ddd20b
stephanezab <stephanezab@gmail.com>
2024-08-28 16:42:09 -0500

    Update Videos.jsx

--- youtube_2.0/src/components/Videos.jsx
+++ youtube_2.0/src/components/Videos.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { Stack, Box} from '@mui/material'
-import {VideoCard, ChannelCard} from './'
+import { Stack, Box } from '@mui/material'
+import { VideoCard, ChannelCard } from './'
 
 
-const Videos = ({videos, direction}) => {
+const Videos = ({ videos, direction }) => {
   if (!videos) return <div>Loading...</div>
   return (
     <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
 
-      {videos.map((item, idx)=> {
+      {videos.map((item, idx) => {
         const hascontent = item.id?.videoId || item.id?.channelId;
 
-        return hascontent? (<Box key={idx} sx={{ width: {xs:'100%',sm:'358px', md: '320px'}, maxWidth:'400', border:'solid 10px red'}}>
-          {item?.id?.videoId && <VideoCard video={item}/>}
-          {item?.id?.channelId && <ChannelCard channelDetail={item}/>}
-        </Box>
-        ): null
+        return hascontent ?
+          (<Box key={idx} sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, maxWidth: '400' }}>
+
+            {item?.id?.videoId && <VideoCard video={item} />}
+
+            {item?.id?.channelId && <ChannelCard channelDetail={item} />}
+            
+          </Box>
+          ) : null
       })}
 
     </Stack>
